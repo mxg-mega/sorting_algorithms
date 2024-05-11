2ee99684ae547d6191309aa62e5cd03c248e3c71
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-05-11 16:35:43 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,30 @@
 #include "sort.h"
+
+/**
+  * bubble_sort - sorts an array of integers
+  * in ascending order using the Bubble sort algorithm
+  * @array: the Array to Sort
+  * @size: size of the array
+  *
+  * Return:
+  */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i = 0, j = 0;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		for (j = 0; j < (size - i - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int swap = 0;
+
+				swap = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = swap;
+				print_array(array, size);
+			}
+		}
+	}
+}
