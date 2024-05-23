0d65afbd25dd1a9922effd023ef5a4c738288073
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-05-23 14:27:14 +0000

    selection sort

--- sort.h
+++ sort.h
@@ -25,7 +25,6 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void swap_nodes(listint_t **head, listint_t *node1, listint_t *node2);
-
+void selection_sort(int *array, size_t size);
 
 #endif /*_SORT_H_*/
